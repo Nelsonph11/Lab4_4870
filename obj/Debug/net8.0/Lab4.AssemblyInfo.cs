780d0644407cdccc3237c3681dcdca238ae83c0a
Nelsonph11 <npetersonhui@my.bcit.ca>
2024-01-30 15:13:17 -0800

    added appsettings to gitignore

--- obj/Debug/net8.0/Lab4.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lab4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7171e5af8d1dce8310c7ee9f3e0c0d795a8fed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
